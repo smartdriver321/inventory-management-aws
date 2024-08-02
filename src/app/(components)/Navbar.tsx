40374ff2a6ffd4626f2480d30c895fb70279cd95
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-02 16:06:37 +0700

    Update Navbar component: Add toggleSidebar function

--- src/app/(components)/Navbar.tsx
+++ src/app/(components)/Navbar.tsx
@@ -4,14 +4,26 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Bell, Menu, Settings, Sun } from 'lucide-react'
 
+import { useAppDispatch, useAppSelector } from '../redux'
+import { setIsSidebarCollapsed } from '@/state'
+
 export default function Navbar() {
+	const dispatch = useAppDispatch()
+	const isSidebarCollapsed = useAppSelector(
+		(state) => state.global.isSidebarCollapsed
+	)
+
+	const toggleSidebar = () => {
+		dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+	}
+
 	return (
 		<div className='flex justify-between items-center w-full mb-7'>
 			{/* LEFT SIDE */}
 			<div className='flex justify-between items-center gap-5'>
 				<button
 					className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100'
-					onClick={() => {}}
+					onClick={toggleSidebar}
 				>
 					<Menu className='w-4 h-4' />
 				</button>
