9fc4c322b5e6b79e92d76eccd4fdfc1f8331a563
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-03 18:24:38 +0700

    Add CardPopularProducts component to Dashboard page

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import CardPopularProducts from './CardPopularProducts'
+
 export default function Dashboard() {
 	return (
 		<div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows'>
-			CardPopularProducts
+			<CardPopularProducts />
 		</div>
 	)
 }
