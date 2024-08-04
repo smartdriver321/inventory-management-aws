d300f44c9f55a83ba3c607d0f12b112edc3efae8
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-04 16:33:16 +0700

    Add useGetUsersQuery to api.ts

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -23,6 +23,12 @@ export interface NewProduct {
 	stockQuantity: number
 }
 
+export interface User {
+	userId: string
+	name: string
+	email: string
+}
+
 export interface SalesSummary {
 	salesSummaryId: string
 	totalValue: number
@@ -50,16 +56,10 @@ export interface ExpenseByCategorySummary {
 	date: string
 }
 
-export interface User {
-	userId: string
-	name: string
-	email: string
-}
-
 export const api = createApi({
 	baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
 	reducerPath: 'api',
-	tagTypes: ['DashboardMetrics', 'Products'],
+	tagTypes: ['DashboardMetrics', 'Products', 'Users'],
 	endpoints: (build) => ({
 		getDashboardMetrics: build.query<DashboardMetrics, void>({
 			query: () => '/dashboard',
@@ -82,6 +82,11 @@ export const api = createApi({
 			}),
 			invalidatesTags: ['Products'],
 		}),
+
+		getUsers: build.query<User[], void>({
+			query: () => '/users',
+			providesTags: ['Users'],
+		}),
 	}),
 })
 
@@ -89,4 +94,5 @@ export const {
 	useGetDashboardMetricsQuery,
 	useGetProductsQuery,
 	useCreateProductMutation,
+	useGetUsersQuery,
 } = api
