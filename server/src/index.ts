52ce4bfff32bea33b001c7c6d42913a788673da6
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-04 17:05:09 +0700

    Create expenses API

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,7 @@ import morgan from 'morgan'
 import dashboardRoutes from './routes/dashboardRoutes'
 import productRoutes from './routes/productRoutes'
 import userRoutes from './routes/userRoutes'
+import expenseRoutes from './routes/expenseRoutes'
 
 /* CONFIGURATIONS */
 dotenv.config()
@@ -26,6 +27,7 @@ app.use(morgan('common'))
 app.use('/dashboard', dashboardRoutes) // http://localhost:8000/dashboard
 app.use('/products', productRoutes) // http://localhost:8000/products
 app.use('/users', userRoutes) // http://localhost:8000/users
+app.use('/expenses', expenseRoutes) // http://localhost:8000/expenses
 
 /* SERVER */
 app.listen(port, '0.0.0.0', () => {
